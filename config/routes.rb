f0a3d9cac0248d29561ff8056893e3ab7a79129f
keithfrancisb <keithfrancisb@gmail.com>
2019-02-04 16:03:43 -0500

    create session and users controller. users show. setup routes for them.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   root 'static_page#root'
 
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+    resource :session, only: [:create, :destroy]
+  end
 end
