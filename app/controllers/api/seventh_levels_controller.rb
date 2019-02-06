88f178225be59e3e4e971c3e49255301b69652b7
jessicali <lij8180@gmail.com>
2019-02-06 18:34:06 -0500

    Create controller for seventh_level

---
+++ app/controllers/api/seventh_levels_controller.rb
@@ -0,0 +1,64 @@
+class Api::SeventhLevelsController < ApplicationController
+  def index
+    @seventh_levels = current_user.seventh_levels
+    render :index
+  end
+
+  def show
+    @seventh_level = SeventhLevel.find(params[:id])
+    render :show
+  end
+
+  def create
+    @seventh_level = SeventhLevel.create(seventh_level_params)
+    if @seventh_level.save
+      render :show
+    else
+      render json: @seventh_level.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @seventh_level = SeventhLevel.find(params[:id])
+    if @seventh_level.update(seventh_level_params)
+      render json: @seventh_level
+    else
+      render json: @seventh_level.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def seventh_levels_params
+    params.require(:seventh_level).permit(
+    :phrase,
+    :core1,
+    :core2,
+    :core3,
+    :core4,
+    :core5,
+    :action1,
+    :question1,
+    :goal1,
+    :action2,
+    :question2,
+    :goal2,
+    :action3,
+    :question3,
+    :goal3,
+    :action4,
+    :question4,
+    :goal4,
+    :action5,
+    :question5,
+    :goal5,
+    :action6,
+    :question6,
+    :goal6,
+    :action7,
+    :question7,
+    :goal7,
+    :user_id)
+  end
+
+end
