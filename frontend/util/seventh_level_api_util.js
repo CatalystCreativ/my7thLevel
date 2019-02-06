d58f0370802bb20e350eb91e1bf2408f136af6e8
Avichai <avisvazana@gmail.com>
2019-02-06 18:42:36 -0500

    Add seven level API Util

---
+++ frontend/util/seventh_level_api_util.js
@@ -0,0 +1,37 @@
+export const createSeventhLevel = (seventhLevelData) => {
+  return $.ajax({
+    method: 'POST',
+    url: 'api/seventh_levels',
+    data: { seventhLevelData }
+  });
+};
+
+
+export const fetchSeventhLevel = (id) => {
+  return $.ajax({
+    method: 'GET',
+    url: `api/seventh_levels/${id}`
+  });
+};
+
+export const fetchSeventhLevels = () => {
+  return $.ajax({
+    method: 'GET',
+    url: 'api/seventh_levels'
+  });
+};
+
+export const updateSeventhLevel = (seventhLevelData) => {
+  return $.ajax({
+    method: 'PATCH',
+    url: `api/seventh_levels/${seventhLevelData.id}`,
+    data: { seventhLevelData }
+  });
+};
+
+export const deleteSeventhLevel = (id) => {
+  return $.ajax({
+    method: 'DESTROY',
+    url: `api/seventh_levels/${id}`
+  });
+};
