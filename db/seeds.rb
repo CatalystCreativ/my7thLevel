116f11345c9900c9bd89c9f19614316ca7d2a526
jessicali <lij8180@gmail.com>
2019-02-06 18:44:54 -0500

    Seed test seventhlevel

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,37 @@
 
 User.destroy_all
 
+SeventhLevel.destroy_all
+
 # Users
 user1 = User.create!(first_name:'Demo',last_name:'User',email:'demo_user@notMail.com',password:'123456', organization:'TheBestOrganization')
+
+seventh_level1 = SeventhLevel.create!(
+  phrase: 'phrase1',
+  core1: 'core1',
+  core2: 'core2',
+  core3: 'core3',
+  core4: 'core4',
+  core5: 'core5',
+  action1: 'action1',
+  question1: 'question1',
+  goal1: 'goal1',
+  action2: 'action2',
+  question2: 'question2',
+  goal2: 'goal2',
+  action3: 'action3',
+  question3: 'question3',
+  goal3: 'goal3',
+  action4: 'action4',
+  question4: 'question4',
+  goal4: 'goal4',
+  action5: 'action5',
+  question5: 'question5',
+  goal5: 'goal5',
+  action6: 'action6',
+  question6: 'question6',
+  goal6: 'goal6',
+  action7: 'action7',
+  question7: 'question7',
+  goal7: 'goal7',
+  user_id: user1.id)
