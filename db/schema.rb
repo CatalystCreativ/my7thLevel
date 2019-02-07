537068c629e704adeb1ff173a9672ac311fafde5
jessicali <lij8180@gmail.com>
2019-02-07 18:10:42 -0500

    Migrate notes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_06_230236) do
+ActiveRecord::Schema.define(version: 2019_02_07_230737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_02_06_230236) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "notes"
     t.index ["user_id"], name: "index_seventh_levels_on_user_id"
   end
 
