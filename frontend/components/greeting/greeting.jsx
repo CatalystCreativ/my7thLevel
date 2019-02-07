6ae4ca939daa933a08a5541c6d395443fae4730a
keithfrancisb <keithfrancisb@gmail.com>
2019-02-06 20:15:22 -0500

    adjust positions of nav items

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -6,25 +6,23 @@ class Greeting extends React.Component {
 
   render() {
     const { currentUser, logout } = this.props;
-    let output;
+    let navItems;
     if (currentUser) {
-      output = (
-        <div className='welcome-msg'>
-          <span>Logged in as {currentUser.first_name}</span>
-          <button onClick={logout}>Log Out</button>
+      navItems = (
+        <div className='d-flex flex-row'>
+          <span className='navbar-text'>Logged in as {currentUser.first_name}</span>
+          <button className='btn btn-outline-primary ml-3' onClick={logout}>Log Out</button>
         </div>
       );
     } else {
-      output = (
-        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
-          <Link className='navbar-brand' to='/'>7th Level</Link>
-
-          <button className='navbar-toggler' type='button' dataToggle='collapse' dataTarget='#navbarNav' ariaControls='navbaNav' ariaExpanded='false' ariaLabel='Toggle navigation'>
+      navItems = (
+        <div>
+          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbaNav' aria-expanded='false' aria-label='Toggle navigation'>
             <span className="navbar-toggler-icon"></span>
           </button>
 
           <div className='collapse navbar-collapse' id='navbarNav'>
-            <ul className='navbar-nav mr-auto'>
+            <ul className='navbar-nav'>
               <li className='nav-item'>
                 <Link className='nav-link' to='/signup'>Sign Up</Link>
               </li>
@@ -33,13 +31,17 @@ class Greeting extends React.Component {
               </li>
             </ul>
           </div>
-        </nav>
+        </div>
       );
     }
 
     return (
       <div>
-        {output}
+        <nav className='justify-content-between navbar navbar-expand-lg navbar-light bg-light'>
+          <Link className='navbar-brand' to='/'>7th Level</Link>
+
+          {navItems}
+        </nav>
       </div>
     );
   }
