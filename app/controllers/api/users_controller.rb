c54215dcaa8f0e3a2ba695c3d7f72f82e28ec904
Avichai <avisvazana@gmail.com>
2019-02-10 15:36:23 -0500

    Add create to user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -10,6 +10,38 @@ class Api::UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      @user.seventh_levels += SeventhLevel.create({
+        :title => '',
+        :phrase => '',
+        :core1 => '',
+        :core2 => '',
+        :core3 => '',
+        :core4 => '',
+        :core5 => '',
+        :action1 => '',
+        :question1 => '',
+        :goal1 => '',
+        :action2 => '',
+        :question2 => '',
+        :goal2 => '',
+        :action3 => '',
+        :question3 => '',
+        :goal3 => '',
+        :action4 => '',
+        :question4 => '',
+        :goal4 => '',
+        :action5 => '',
+        :question5 => '',
+        :goal5 => '',
+        :action6 => '',
+        :question6 => '',
+        :goal6 => '',
+        :action7 => '',
+        :question7 => '',
+        :goal7 => '',
+        :user_id => '',
+        :notes => ''
+      })
       login(@user)
       render 'api/users/show'
     else
