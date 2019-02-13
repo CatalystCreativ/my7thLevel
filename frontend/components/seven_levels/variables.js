1ea91316c2a84cf742189a0b7289cc31c177c731
keithfrancisb <keithfrancisb@gmail.com>
2019-02-13 10:03:58 -0500

    removed enumeration in examples. will use ol in components. not ul.

--- frontend/components/seven_levels/variables.js
+++ frontend/components/seven_levels/variables.js
@@ -15,20 +15,20 @@ const LEVEL_ONE = {
   levelDescription: "Characterized by a person showing disinterest; they're completely idle and avoid tasks,interactions or attempts at communication",
   actionDescription: "Define the action that represents this level for you/your company",
   actionExamples: [
-    "1. A six year old ignores their teacher when they ask a question to the class.",
-    "2. My employee is not acknowleging or opening my emails.",
-    "3. No one is clicking on our instagram ads."
+    "A six year old ignores their teacher when they ask a question to the class.",
+    "My employee is not acknowleging or opening my emails.",
+    "No one is clicking on our instagram ads."
   ],
   questionDescription: "List the Questions associated with those actions",
   questionExamples: [
-    "1. Have I ever asked my employees how they like to be communicated with?",
-    "2. Am I reaching out during the right time of day/week?",
-    "3. Does my target audience use Instagram?"
+    "Have I ever asked my employees how they like to be communicated with?",
+    "Am I reaching out during the right time of day/week?",
+    "Does my target audience use Instagram?"
   ],
   goalDescription: "Choose 1-2 goals for the next month or quarter",
   goalExamples: [
-    "1. Conduct an informal survey to find out whether my audience perfers emails or text messages.",
-    "2.Test ads in three different platforms to determine the best option for reaching my audience."
+    "Conduct an informal survey to find out whether my audience perfers emails or text messages.",
+    "Test ads in three different platforms to determine the best option for reaching my audience."
   ]
 };
 
@@ -38,20 +38,20 @@ const LEVEL_TWO = {
   levelDescription: "Characterized by a person\'s confusion and feelings of inadequency\; they don't understand an element of your message so they give up quickly.",
   actionDescription: "Define the action that represents this level for you/your company",
   actionExamples: [
-    "1. A 6-year old doesn't raise their hand in class because they don't understand the question.",
-    "2. I can see people read my email, but no one reached out for more information.",
-    "3. Audience are visiting my website, but don't make it past the homepage."
+    "A 6-year old doesn't raise their hand in class because they don't understand the question.",
+    "I can see people read my email, but no one reached out for more information.",
+    "Audience are visiting my website, but don't make it past the homepage."
   ],
   questionDescription: "List the Questions associated with those actions",
   questionExamples: [
-    "1. Do I use jargon or technical language too often?",
-    "2. Is my messaging clear concise and simple?",
-    "3. Which version of ad copy is easiest to understand for my audience?"
+    "Do I use jargon or technical language too often?",
+    "Is my messaging clear concise and simple?",
+    "Which version of ad copy is easiest to understand for my audience?"
   ],
   goalDescription: "Choose 1-2 goals for the next month or quarter",
   goalExamples: [
-    "1. Test email content that is written vs explained in a short video.",
-    "2. Decrease my website bounce rate."
+    "Test email content that is written vs explained in a short video.",
+    "Decrease my website bounce rate."
   ]
 };
 
@@ -61,21 +61,21 @@ const LEVEL_THREE = {
   levelDescription: "Characterized by a person's interest and understanding of your message, but loss of interest due to distraction or inability to complete a task.",
   actionDescription: "Define the action that represents this level for you/your company",
   actionExamples: [
-    "1. A 6-year old is in interested in their assignment until they can't use their favorite marker.",
-    "2. Employees express interest in joining the weight loss program, but then no one finishes the steps to sign up.",
-    "3. Audiences click on the purchase button in my ad, but then don't get to my website.",
-    "4. A friend shows interest in spending time with you, but you're always busy."
+    "A 6-year old is in interested in their assignment until they can't use their favorite marker.",
+    "Employees express interest in joining the weight loss program, but then no one finishes the steps to sign up.",
+    "Audiences click on the purchase button in my ad, but then don't get to my website.",
+    "A friend shows interest in spending time with you, but you're always busy."
   ],
   questionDescription: "List the Questions associated with those actions",
   questionExamples: [
-    "1. Are their too many steps or are the steps to join too difficult?",
-    "2. Does my website load quickly enough?",
-    "3. Am I rarely available when my friends aske me to join them for activities?"
+    "Are their too many steps or are the steps to join too difficult?",
+    "Does my website load quickly enough?",
+    "Am I rarely available when my friends aske me to join them for activities?"
   ],
   goalDescription: "Choose 1-2 goals for the next month or quarter",
   goalExamples: [
-    "1. Set up a web form to make signing up got my employee program easy.",
-    "2. Work with my developers to decrease the website load time."
+    "Set up a web form to make signing up got my employee program easy.",
+    "Work with my developers to decrease the website load time."
   ]
 };
 
@@ -86,21 +86,21 @@ const LEVEL_FOUR = {
   levelDescription: "Characterized by a person's active response and participation in instruction and activities, provided the barrier to entry and their personal sacrifice is low.",
   actionDescription: "Define the action that represents this level for you/your company",
   actionExamples: [
-    "1. A 6-year old who participates when their teacher asks, 'Raise your hand if you like candy.'.",
-    "2. My employees will attend a non-mandatory virtual training fo HR when askes to, but will not particapate.",
-    "3. My followers will like my post but not share it."
+    "A 6-year old who participates when their teacher asks, 'Raise your hand if you like candy.'.",
+    "My employees will attend a non-mandatory virtual training fo HR when askes to, but will not particapate.",
+    "My followers will like my post but not share it."
   ],
   questionDescription: "List the Questions associated with those actions",
   questionExamples: [
-    "1. Am I providing enough of an incentive or connection that it excites people to participates?",
-    "2. What kind of engagement am I looking for?",
-    "3. What action am I really asking people to take?"
+    "Am I providing enough of an incentive or connection that it excites people to participates?",
+    "What kind of engagement am I looking for?",
+    "What action am I really asking people to take?"
   ],
   goalDescription: "Choose 1-2 goals for the next month or quarter",
   goalExamples: [
-    "1. Increase employee participation in meetings by 20%.",
-    "2. Increase my Facebook engagement rate by 5%.",
-    "3. Launch a sweepstakes that includes a requirement to share, commit and post."
+    "Increase employee participation in meetings by 20%.",
+    "Increase my Facebook engagement rate by 5%.",
+    "Launch a sweepstakes that includes a requirement to share, commit and post."
   ]
 };
 
@@ -110,21 +110,21 @@ const LEVEL_FIVE = {
   levelDescription: "Characterized by a person's genuine excitement and interest in a message because it fulfills an element of their own self/personal interests; their engagement is driven by self-interest, not an interest in the specific individual or a company.",
   actionDescription: "Define the action that represents this level for you/your company",
   actionExamples: [
-    "1. A 6-year old who participates in class to earn extra credit or a reward for completion.",
-    "2. My employees will volunteer for a special project if they'll earn an extra vacation day.",
-    "3. People will comment on my post if they can win ticjets ti meet their favorite influencer."
+    "A 6-year old who participates in class to earn extra credit or a reward for completion.",
+    "My employees will volunteer for a special project if they'll earn an extra vacation day.",
+    "People will comment on my post if they can win ticjets ti meet their favorite influencer."
   ],
   questionDescription: "List the Questions associated with those actions",
   questionExamples: [
-    "1. Am I offering the right incentive to boost the type of engagement I am looking for?",
-    "2. How am I leveraging this giveaway or partnership to increase brand awareness or drive traffic?",
-    "3. How am I going to maximize people's participation after the incentive?"
+    "Am I offering the right incentive to boost the type of engagement I am looking for?",
+    "How am I leveraging this giveaway or partnership to increase brand awareness or drive traffic?",
+    "How am I going to maximize people's participation after the incentive?"
   ],
   goalDescription: "Choose 1-2 goals for the next month or quarter",
   goalExamples: [
-    "1. Indentify the mutually beneficial narrative prior to any partnership to ensure that the partner is communicating the brand value to their audience in an integrative approach.",
-    "2. Increase website traffic or email acquisition from partnership (incentives) channels.",
-    "3. Launch a program that pays for masters courses that align with company goals, to recieve that benefit employees must dedicate a certain number of years of service."
+    "Indentify the mutually beneficial narrative prior to any partnership to ensure that the partner is communicating the brand value to their audience in an integrative approach.",
+    "Increase website traffic or email acquisition from partnership (incentives) channels.",
+    "Launch a program that pays for masters courses that align with company goals, to recieve that benefit employees must dedicate a certain number of years of service."
 ]
 };
 
@@ -135,22 +135,22 @@ const LEVEL_SIX = {
   levelDescription: "Characterized by a person's inspiration to make a change in their own life; taking a personal action to set goals and transform their lives to achieve an objective based on a message or interaction.",
   actionDescription: "Define the action that represents this level for you/your company",
   actionExamples: [
-  "1. A 6-year old who learns how sea turtles are affected by plastic in the ocean and recycles for the rest of their life.",
-  "2. My team has committed to a weaght loss and health goal.",
-  "3. Our hotel is committed to sustainability and we hear stories of how our guests change their habits after staying in our property and seeing how we manage things.",
-  "4. My mentor shared her story of how shortening her meetings to 45 minutes changed her work life and it inspired me to do the same."
+  "A 6-year old who learns how sea turtles are affected by plastic in the ocean and recycles for the rest of their life.",
+  "My team has committed to a weaght loss and health goal.",
+  "Our hotel is committed to sustainability and we hear stories of how our guests change their habits after staying in our property and seeing how we manage things.",
+  "My mentor shared her story of how shortening her meetings to 45 minutes changed her work life and it inspired me to do the same."
 ],
   questionDescription: "List the Questions associated with those actions",
   questionExamples: [
-    "1. What do I stand for?",
-    "2. Why does what I or my comany matter?",
-    "3. How is our brand helping others?"
+    "What do I stand for?",
+    "Why does what I or my comany matter?",
+    "How is our brand helping others?"
   ],
   goalDescription: "Choose 1-2 goals for the next month or quarter",
   goalExamples: [
-    "1. Increase positive testimonials and/or reviews.",
-    "2. Develop a user-generated content social media campaign.",
-    "3. Help at least 3 other people set their personal development goals."
+    "Increase positive testimonials and/or reviews.",
+    "Develop a user-generated content social media campaign.",
+    "Help at least 3 other people set their personal development goals."
   ]
 };
 
@@ -160,21 +160,21 @@ const LEVEL_SEVEN = {
   levelDescription: "Characterized by a person's reflection using prior knowledge, experience, feeling and values; a direct alignment with their personal beliefs leading to loyalty and action without incentive.",
   actionDescription: "Define the action that represents this level for you/your company",
   actionExamples: [
-    "1. A 6-year old loves Matilda because she saw herself in the character, so she relates to herseld and her life experiences with Matilda.",
-    "2. My employees will tell anyone they meet about the company as though we are a family.",
-    "3. My costumers post about my products without me asking them to."
+    "A 6-year old loves Matilda because she saw herself in the character, so she relates to herseld and her life experiences with Matilda.",
+    "My employees will tell anyone they meet about the company as though we are a family.",
+    "My costumers post about my products without me asking them to."
   ],
   questionDescription: "List the Questions associated with those actions",
   questionExamples: [
-    "1. How am I delighting this level of people?",
-    "2. How am I using my message to emotionally connect with people?",
-    "3. How am I individualizing customers loyalty, based on personal values and beliefs?"
+    "How am I delighting this level of people?",
+    "How am I using my message to emotionally connect with people?",
+    "How am I individualizing customers loyalty, based on personal values and beliefs?"
   ],
   goalDescription: "Choose 1-2 goals for the next month or quarter",
   goalExamples: [
-    "1. Increase employee retention.",
-    "2. Develop an outreach program to thank raving fans.",
-    "3. Share user-generated content."
+    "Increase employee retention.",
+    "Develop an outreach program to thank raving fans.",
+    "Share user-generated content."
   ]
 };
 
