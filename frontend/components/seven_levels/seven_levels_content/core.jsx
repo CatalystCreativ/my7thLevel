dc897f36a8aee09377c361e47dea80c68a8d7445
keithfrancisb <keithfrancisb@gmail.com>
2019-02-14 20:58:31 -0500

    fix Core component to accomodate new changes in seventh_level component

--- frontend/components/seven_levels/seven_levels_content/core.jsx
+++ frontend/components/seven_levels/seven_levels_content/core.jsx
@@ -2,25 +2,24 @@ import { CORE } from '../variables';
 import React from 'react';
 
 class Core extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.props.handleSubmit;
-    this.update = this.props.updateField;
-  }
+  
 
   render() {
 
-    const {core1, core2, core3, core4, core5, phrase} = this.props.core
-    const core = [core1, core2, core3, core4, core5, phrase];
-    CORE.map((question, idx) => {
-      core.push(
-        <div id={`question${idx}`}>
+    const { update, handleSubmit } = this.props;
+    const { core1, core2, core3, core4, core5, phrase } = this.props;
+    // make array to complement the mapping procedure
+    const core = [core1, core2, core3, core4, core5, phrase]; 
+
+    const coreTexts = CORE.map((question, idx) => {
+      return (
+        <div key={idx} id={`question${idx}`}>
           <div className="card col-5">
             <div className="card-header">
               {question}
             </div>
             <div className="card-body">
-              <textarea className="w-200" name="" id="" cols="30" rows="10" value={core[idx]}></textarea>
+              <textarea onChange={update(`core${idx+1}`)} className="w-100" name="" id="" cols="30" rows="2" value={core[idx]}></textarea>
             </div>
           </div>
         </div>
@@ -29,32 +28,10 @@ class Core extends React.Component {
 
     return (
       <div>
-        {core}
+        {coreTexts}
       </div>
     );
   }
 }
-
-{/* <form onSubmit={this.handleSubmit} className="">
-  {CORE.core1}
-  <div>{this.level.levelDescription}</div>
-
-  <div className="~~~~~">
-    <h2>
-      <label>ACTION</label>
-      {this.props.actionDescription}
-    </h2>
-  </div>
-
-  <div className="form-group">
-    <label htmlFor="action">Action: {this.level.actionDescription}</label>
-    <input type="text"
-      className="form-control"
-      placeholder="Email Address"
-      id="email"
-      value={this.state.email}
-      onChange={this.update(`action${this.props.core}`)}/>
-  </div>
-</form> */}
 
 export default Core;
