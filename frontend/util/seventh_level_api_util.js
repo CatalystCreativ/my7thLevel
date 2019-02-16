9368db6204d93bb7dac25124d6eb019ed1670217
keithfrancisb <keithfrancisb@gmail.com>
2019-02-16 17:59:07 -0500

    make createSeventhLevel request work

--- frontend/util/seventh_level_api_util.js
+++ frontend/util/seventh_level_api_util.js
@@ -1,8 +1,38 @@
-export const createSeventhLevel = (seventhLevelData) => {
+export const createSeventhLevel = () => {
+  const seventh_level = {
+    title: "New Seventh Level",
+    phrase: "",
+    core1: "",
+    core2: "",
+    core3: "",
+    core4: "",
+    core5: "",
+    action1: "",
+    question1: "",
+    goal1: "",
+    action2: "",
+    question2: "",
+    goal2: "",
+    action3: "",
+    question3: "",
+    goal3: "",
+    action4: "",
+    question4: "",
+    goal4: "",
+    action5: "",
+    question5: "",
+    goal5: "",
+    action6: "",
+    question6: "",
+    goal6: "",
+    action7: "",
+    question7: "",
+    goal7: "",
+  };
   return $.ajax({
     method: 'POST',
     url: 'api/seventh_levels',
-    data: { seventhLevelData }
+    data: { seventh_level }
   });
 };
 
