37e1ac39152d2bf0d00cdf82a084beefae745be4
Avichai <avisvazana@gmail.com>
2019-04-05 15:43:39 -0400

    Changed font size, borders, boldness, margins

--- frontend/components/seven_levels/seven_levels_content/core.jsx
+++ frontend/components/seven_levels/seven_levels_content/core.jsx
@@ -15,11 +15,13 @@ class Core extends React.Component {
       return (
         <>
           <li key={idx} className='list-group-item-dark py-2 px-3' id={`question${idx}`}>
-            {question}
+            <strong>{question}</strong>
           </li>
           <li key={idx+1} className='list-group-item-light py-2 px-5'>
             <input className='w-100' onChange={update(`core${idx + 1}`)}type="text" placeholder={CORE_PLACEHOLDER[idx]} value={core[idx]}></input>
           </li>
+          <br></br>
+          <br></br>
         </>
       );
     });
@@ -31,7 +33,7 @@ class Core extends React.Component {
         </ul>
         <div className='container mb-4'>
           <div className='row justify-content-center flex-row'>
-            <button className='btn btn-back ml-3 px-4' type='submit'>Save</button>
+            <button className='btn btn-back ml-3 px-4' type='submit'><strong>SAVE</strong></button>
           </div>
         </div>
       </div>
