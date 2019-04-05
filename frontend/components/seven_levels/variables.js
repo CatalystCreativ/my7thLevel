ec9cd954240851636ea772ee94efa46ea724f8f9
Avichai <avisvazana@gmail.com>
2019-04-05 14:56:14 -0400

    Change colors

--- frontend/components/seven_levels/variables.js
+++ frontend/components/seven_levels/variables.js
@@ -25,19 +25,19 @@ const LEVEL_ONE = {
   levelNum: "1",
   levelTitle: "DISENGAGEMENT",
   levelDescription: "Characterized by a person showing disinterest; they're completely idle and avoid tasks, interactions or attempts at communication",
-  actionDescription: "Define the action that represents this level for you/your company",
+  actionDescription: "DEFINE THE ACTION THAT REPRESENTS THIS LEVEL FOR YOU/YOUR COMPANY",
   actionExamples: [
     "A six year old ignores their teacher when they ask a question to the class.",
     "My employee is not acknowledging or opening my emails.",
     "No one is clicking on our instagram ads."
   ],
-  questionDescription: "List the questions associated with those actions",
+  questionDescription: "LIST THE QUESTIONS ASSOCIATED WITH THAT ACTION",
   questionExamples: [
     "Have I ever asked my employees how they like to be communicated with?",
     "Am I reaching out during the right time of day/week?",
     "Does my target audience use Instagram?"
   ],
-  goalDescription: "Choose 1-2 goals for the next month or quarter",
+  goalDescription: "CHOOSE 1-2 GOALS FOR THE NEXT MONTH OR QUARTER",
   goalExamples: [
     "Conduct an informal survey to find out whether my audience prefers emails or text messages.",
     "Test ads in three different platforms to determine the best option for reaching my audience."
@@ -48,19 +48,19 @@ const LEVEL_TWO = {
   levelNum: "2",
   levelTitle: "UNSYSTEMATIC ENGAGEMENT",
   levelDescription: "Characterized by a person\'s confusion and feelings of inadequacy\; they don't understand an element of your message so they give up quickly.",
-  actionDescription: "Define the action that represents this level for you/your company",
+  actionDescription: "DEFINE THE ACTION THAT REPRESENTS THIS LEVEL FOR YOU/YOUR COMPANY",
   actionExamples: [
     "A 6-year old doesn't raise their hand in class because they don't understand the question.",
     "I can see people read my email, but no one reached out for more information.",
     "Audience are visiting my website, but don't make it past the homepage."
   ],
-  questionDescription: "List the questions associated with those actions",
+  questionDescription: "LIST THE QUESTIONS ASSOCIATED WITH THAT ACTION",
   questionExamples: [
     "Do I use jargon or technical language too often?",
     "Is my messaging clear concise and simple?",
     "Which version of ad copy is easiest to understand for my audience?"
   ],
-  goalDescription: "Choose 1-2 goals for the next month or quarter",
+  goalDescription: "CHOOSE 1-2 GOALS FOR THE NEXT MONTH OR QUARTER",
   goalExamples: [
     "Test email content that is written vs explained in a short video.",
     "Decrease my website bounce rate."
@@ -71,20 +71,20 @@ const LEVEL_THREE = {
   levelNum: "3",
   levelTitle: "FRUSTRATED ENGAGEMENT",
   levelDescription: "Characterized by a person's interest and understanding of your message, but loss of interest due to distraction or inability to complete a task.",
-  actionDescription: "Define the action that represents this level for you/your company",
+  actionDescription: "DEFINE THE ACTION THAT REPRESENTS THIS LEVEL FOR YOU/YOUR COMPANY",
   actionExamples: [
     "A 6-year old is in interested in their assignment until they can't use their favorite marker.",
     "Employees express interest in joining the weight loss program, but then no one finishes the steps to sign up.",
     "Audiences click on the purchase button in my ad, but then don't get to my website.",
     "A friend shows interest in spending time with you, but you're always busy."
   ],
-  questionDescription: "List the questions associated with those actions",
+  questionDescription: "LIST THE QUESTIONS ASSOCIATED WITH THAT ACTION",
   questionExamples: [
     "Are their too many steps or are the steps to join too difficult?",
     "Does my website load quickly enough?",
     "Am I rarely available when my friends asks me to join them for activities?"
   ],
-  goalDescription: "Choose 1-2 goals for the next month or quarter",
+  goalDescription: "CHOOSE 1-2 GOALS FOR THE NEXT MONTH OR QUARTER",
   goalExamples: [
     "Set up a web form to make signing up got my employee program easy.",
     "Work with my developers to decrease the website load time."
@@ -96,19 +96,19 @@ const LEVEL_FOUR = {
   levelNum: "4",
   levelTitle: "STRUCTURE-DEPENDENT ENGAGEMENT",
   levelDescription: "Characterized by a person's active response and participation in instruction and activities, provided the barrier to entry and their personal sacrifice is low.",
-  actionDescription: "Define the action that represents this level for you/your company",
+  actionDescription: "DEFINE THE ACTION THAT REPRESENTS THIS LEVEL FOR YOU/YOUR COMPANY",
   actionExamples: [
     "A 6-year old who participates when their teacher asks, 'Raise your hand if you like candy.'.",
     "My employees will attend a non-mandatory virtual training fo HR.",
     "My followers will like my post but not share it."
   ],
-  questionDescription: "List the questions associated with those actions",
+  questionDescription: "LIST THE QUESTIONS ASSOCIATED WITH THAT ACTION",
   questionExamples: [
     "Am I providing enough of an incentive or connection that it excites people to participates?",
     "What kind of engagement am I looking for?",
     "What action am I really asking people to take?"
   ],
-  goalDescription: "Choose 1-2 goals for the next month or quarter",
+  goalDescription: "CHOOSE 1-2 GOALS FOR THE NEXT MONTH OR QUARTER",
   goalExamples: [
     "Increase employee participation in meetings by 20%.",
     "Increase my Facebook engagement rate by 5%.",
@@ -120,19 +120,19 @@ const LEVEL_FIVE = {
   levelNum: "5",
   levelTitle: "SELF-REGULATED INTEREST",
   levelDescription: "Characterized by a person's genuine excitement and interest in a message because it fulfills an element of their own self/personal interests; their engagement is driven by self-interest, not an interest in the specific individual or a company.",
-  actionDescription: "Define the action that represents this level for you/your company",
+  actionDescription: "DEFINE THE ACTION THAT REPRESENTS THIS LEVEL FOR YOU/YOUR COMPANY",
   actionExamples: [
     "A 6-year old who participates in class to earn extra credit or a reward for completion.",
     "My employees will volunteer for a special project if they'll earn an extra vacation day.",
     "People will comment on my post if they can win tickets ti meet their favorite influencer."
   ],
-  questionDescription: "List the questions associated with those actions",
+  questionDescription: "LIST THE QUESTIONS ASSOCIATED WITH THAT ACTION",
   questionExamples: [
     "Am I offering the right incentive to boost the type of engagement I am looking for?",
     "How am I leveraging this giveaway or partnership to increase brand awareness or drive traffic?",
     "How am I going to maximize people's participation after the incentive?"
   ],
-  goalDescription: "Choose 1-2 goals for the next month or quarter",
+  goalDescription: "CHOOSE 1-2 GOALS FOR THE NEXT MONTH OR QUARTER",
   goalExamples: [
     "Identify the mutually beneficial narrative prior to any partnership to ensure that the partner is communicating the brand value to their audience in an integrative approach.",
     "Increase website traffic or email acquisition from partnership (incentives) channels.",
@@ -145,20 +145,20 @@ const LEVEL_SIX = {
   levelNum: "6",
   levelTitle: "CRITICAL ENGAGEMENT",
   levelDescription: "Characterized by a person's inspiration to make a change in their own life; taking a personal action to set goals and transform their lives to achieve an objective based on a message or interaction.",
-  actionDescription: "Define the action that represents this level for you/your company",
+  actionDescription: "DEFINE THE ACTION THAT REPRESENTS THIS LEVEL FOR YOU/YOUR COMPANY",
   actionExamples: [
   "A 6-year old who learns how sea turtles are affected by plastic in the ocean and recycles for the rest of their life.",
   "My team has committed to a weight loss and health goal.",
   "Our hotel is committed to sustainability and we hear stories of how our guests change their habits after staying in our property and seeing how we manage things.",
   "My mentor shared her story of how shortening her meetings to 45 minutes changed her work life and it inspired me to do the same."
 ],
-  questionDescription: "List the questions associated with those actions",
+  questionDescription: "LIST THE QUESTIONS ASSOCIATED WITH THAT ACTION",
   questionExamples: [
     "What do I stand for?",
     "Why does what I or my company matter?",
     "How is our brand helping others?"
   ],
-  goalDescription: "Choose 1-2 goals for the next month or quarter",
+  goalDescription: "CHOOSE 1-2 GOALS FOR THE NEXT MONTH OR QUARTER",
   goalExamples: [
     "Increase positive testimonials and/or reviews.",
     "Develop a user-generated content social media campaign.",
@@ -170,19 +170,19 @@ const LEVEL_SEVEN = {
   levelNum: "7",
   levelTitle: "LITERATE THINKING",
   levelDescription: "Characterized by a person's reflection using prior knowledge, experience, feeling and values; a direct alignment with their personal beliefs leading to loyalty and action without incentive.",
-  actionDescription: "Define the action that represents this level for you/your company",
+  actionDescription: "DEFINE THE ACTION THAT REPRESENTS THIS LEVEL FOR YOU/YOUR COMPANY",
   actionExamples: [
     "A 6-year old loves Matilda because she saw herself in the character, so she relates to herself and her life experiences with Matilda.",
     "My employees will tell anyone they meet about the company as though we are a family.",
     "My costumers post about my products without me asking them to."
   ],
-  questionDescription: "List the questions associated with those actions",
+  questionDescription: "LIST THE QUESTIONS ASSOCIATED WITH THAT ACTION",
   questionExamples: [
     "How am I delighting this level of people?",
     "How am I using my message to emotionally connect with people?",
     "How am I individualizing customers loyalty, based on personal values and beliefs?"
   ],
-  goalDescription: "Choose 1-2 goals for the next month or quarter",
+  goalDescription: "CHOOSE 1-2 GOALS FOR THE NEXT MONTH OR QUARTER",
   goalExamples: [
     "Increase employee retention.",
     "Develop an outreach program to thank raving fans.",
